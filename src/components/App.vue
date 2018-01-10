264860505ed48dcca9eefe51bd01afcdc03a625b
Sasa Prsir <sashaprsheer@gmail.com>
2018-01-10 16:00:41 +0100

    Start coding the application.

---
+++ src/components/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>{{msg}}</div>
+</template>
+<script>
+  export default {
+    data () {
+      return {
+        msg: 'Hello World 127'
+      }
+    },
+    created () {
+    }
+  }
+</script>
